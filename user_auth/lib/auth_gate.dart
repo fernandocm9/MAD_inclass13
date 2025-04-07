0c89c41c574ca7bb9f72e62779b8ea222101b8ff
Fernando Curiel <fernando@Abigails-Laptop.local>
2025-04-07 13:11:27 -0400

    added firebase files

---
+++ user_auth/lib/auth_gate.dart
@@ -0,0 +1,35 @@
+import 'package:firebase_auth/firebase_auth.dart' hide EmailAuthProvider;
+import 'package:firebase_ui_auth/firebase_ui_auth.dart';
+import 'package:flutter/material.dart';
+
+import 'main.dart';
+
+class AuthGate extends StatelessWidget {
+  const AuthGate({super.key});
+
+  @override
+  Widget build(BuildContext context) {
+    return StreamBuilder<User?>(
+      stream: FirebaseAuth.instance.authStateChanges(),
+      builder: (context, snapshot) {
+        if (!snapshot.hasData) {
+          return SignInScreen(
+            providers: [
+              EmailAuthProvider(),
+            ],
+            headerBuilder: (context, constraints, shrinkOffset) {
+              return Padding(
+                padding: const EdgeInsets.all(20),
+                child: AspectRatio(
+                  aspectRatio: 1,
+                ),
+              );
+            },
+          );
+        }
+
+        return MyHomePage(title: 'Firebase Auth Demo');
+      },
+    );
+  }
+}
